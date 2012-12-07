dfaab5f4998b198550f2be78fb96eed1d92aeeb4
prescience <prescience@prescience-laptop.(none)>
2012-12-07 11:24:44 +0530

    new exercise added

---
+++ operator_overloading.rb
@@ -0,0 +1,30 @@
+class ClassA
+  attr_accessor :value
+ def initialize(value)
+ @value = value
+ end
+def +(obj)
+  c = ClassA.new(self.value + obj.value)
+  return c
+end
+end
+
+class ClassB
+  attr_accessor :value
+ def initialize(value)
+ @value = value
+ end
+def +(obj)
+  c = ClassB.new(self.value + obj.value)
+  return c
+end
+end
+
+a = ClassA.new(10)
+puts "classA intialized : #{a.inspect}"
+b = ClassB.new(20)
+puts "classB intialized : #{b.inspect}"
+c = a + b
+puts "Sum of a + B  : #{c.inspect}"
+puts "Sum of ClassA & ClassB : #{c.value}"
+
